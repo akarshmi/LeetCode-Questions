ff8ad60f185e2221060a0b478ffde9608fe16e3f
Akarsh Mishra <155228025+akarshmi@users.noreply.github.com>
2026-06-22 18:41:29 +0530

    Time: 1 ms (84.43%), Space: 42.6 MB (40.73%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return gcd(n*n,(n*n)+n);                
+    }
+
+    private int gcd(int f,int s){
+        while(s>0){
+            int t = s;
+            s = f%s;
+            f = t;
+        }
+        return f;
+    }
+}
