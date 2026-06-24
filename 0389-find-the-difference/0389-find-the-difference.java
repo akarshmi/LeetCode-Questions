2eb2c7faa4fc13e089592536918f52c521505fb7
Akarsh Mishra <155228025+akarshmi@users.noreply.github.com>
2026-06-24 12:22:56 +0530

    Time: 2 ms (64.67%), Space: 42.8 MB (96.44%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,13 +1,24 @@
 class Solution {
     public static char findTheDifference(String s, String t) {
-        char ans = 0;
+        char s1=0,s2=0;
 
         for (int i = 0; i < s.length(); i++) {
-            ans ^= s.charAt(i);
-            ans ^= t.charAt(i);
+            s1 += s.charAt(i);
+            s2 += t.charAt(i);
         }
-        ans ^= t.charAt(t.length() - 1);
+        s2 += t.charAt(t.length()-1);
 
-        return ans;
+        return (char) (s2-s1);
+
+
+        // char ans = 0;
+        // for (int i = 0; i < s.length(); i++) {
+        //     ans ^= s.charAt(i);
+        //     ans ^= t.charAt(i);
+        // }
+        // ans ^= t.charAt(t.length() - 1);
+
+        // return ans;
+
     }
 }
