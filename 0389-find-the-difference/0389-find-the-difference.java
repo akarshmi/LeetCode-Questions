de78d54dc3eb61157229c283babc741c68d11716
Akarsh Mishra <155228025+akarshmi@users.noreply.github.com>
2026-06-24 12:17:37 +0530

    Time: 2 ms (64.67%), Space: 43.1 MB (53.53%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static char findTheDifference(String s, String t) {
+        char ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            ans ^= s.charAt(i);
+            ans ^= t.charAt(i);
+        }
+        ans ^= t.charAt(t.length() - 1);
+
+        return ans;
+    }
+}
