9738c737cbe52400d674077b6e77ac5db0bd1cda
Akarsh Mishra <155228025+akarshmi@users.noreply.github.com>
2026-06-24 12:01:36 +0530

    Time: 1 ms (89.03%), Space: 43 MB (79.37%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,28 +1,14 @@
 class Solution {
-    public static boolean detectCapitalUse(String word) {
-        int uc = 0;
-        int lc = 0;
-        char[] ch = word.toCharArray();
-        for (char c : ch) {
-            if (c >= 65 && c <= 90) {
-                uc++;
-            } else if (c >= 97 && c <= 122) {
-                lc++;
-            }
+    public boolean detectCapitalUse(String s) {
+        int n = s.length();
+        int c = 0;
+        for( char ch : s.toCharArray()){
+            if(ch >= 'A' && ch <= 'Z') c++;
         }
-
-        System.err.print(lc + "       " + uc);
-        if (lc == 0 && uc == word.length()) {
-            System.err.print(lc + "       " + uc);
-            return true;
-        }
-        if (uc == 0 && lc == word.length()) {
-            return true;
-        }
-        if (uc == 1 && word.charAt(0) >= 'A' && word.charAt(0) <= 'Z') {
-            return true;
-        } else {
-            return false;
+        if(c == n || c == 0) return true;
+        else{
+            if(c == 1 && s.charAt(0) >= 'A' && s.charAt(0) <= 'Z') return true;
+            else return false;
         }
     }
 }
