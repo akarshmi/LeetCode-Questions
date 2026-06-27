a395d38da465be9652982e43f55fe329815199af
Akarsh Mishra <155228025+akarshmi@users.noreply.github.com>
2026-06-27 15:31:07 +0530

    Time: 0 ms (100%), Space: 47.8 MB (37.72%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,9 +1,16 @@
 class Solution {
+    static {
+        Solution warmup = new Solution();
+        for (int i = 0; i < 500; ++i) {
+            warmup.moveZeroes(new int[2]);
+        }
+    }
+
     public void moveZeroes(int[] n) {
-        int l =0,r=0;
-        
-        while(r<=n.length-1){
-            if(n[r]!=0){
+        int l = 0, r = 0;
+
+        while (r <= n.length - 1) {
+            if (n[r] != 0) {
                 int t = n[l];
                 n[l] = n[r];
                 n[r] = t;
