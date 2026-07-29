b33d09c1bc29eb454a9458b2b7940dc93f0b1138
Akarsh Mishra <155228025+akarshmi@users.noreply.github.com>
2026-07-29 19:21:24 +0530

    Time: 1537 ms (19.72%), Space: 48.4 MB (93.95%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int c = 0;
+        for(int i=0; i<nums.length; i++){
+            int currSum = 0;
+            for(int j=i; j<nums.length;j++){
+                currSum +=nums[j];
+                if(currSum == k){
+                    c++;
+                }
+    
+            }
+        }
+        return c;
+    }
+}
