8ec93231eb6f52a0809483a1d950b64076b97ebb
Akarsh Mishra <155228025+akarshmi@users.noreply.github.com>
2026-07-29 10:23:19 +0530

    Time: 2 ms (70.8%), Space: 55.7 MB (46.46%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,29 +1,29 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        // HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int num : nums){
-            map.put(num, map.getOrDefault(num,0)+1);
-        }
-        int max = 0;
-        int m = 0;
-        for(Map.Entry<Integer,Integer> e : map.entrySet()){
-            if(e.getValue()>max){
-                max = e.getValue();
-                m = e.getKey();
-            }
-        }
-            return m;
-
-
-
-        // int c= 0, cnt = 0;
-        // for(int n:nums){
-        //     if(cnt==0){
-        //         c=n;
-        //     }
-        //     cnt+=(n==c) ? 1 : -1;
+        // for(int num : nums){
+        //     map.put(num, map.getOrDefault(num,0)+1);
         // }
-        // return c;
+        // int max = 0;
+        // int m = 0;
+        // for(Map.Entry<Integer,Integer> e : map.entrySet()){
+        //     if(e.getValue()>max){
+        //         max = e.getValue();
+        //         m = e.getKey();
+        //     }
+        // }
+        //     return m;
+
+
+
+        int c= 0, cnt = 0;
+        for(int n:nums){
+            if(cnt==0){
+                c=n;
+            }
+            cnt+=(n==c) ? 1 : -1;
+        }
+        return c;
     }
 }
