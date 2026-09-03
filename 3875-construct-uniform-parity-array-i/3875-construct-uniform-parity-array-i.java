24ef262f894360901e57db956ec6e1862e1c08f3
Akarsh Mishra <155228025+akarshmi@users.noreply.github.com>
2026-09-03 10:56:39 +0530

    Time: 0 ms (100%), Space: 45 MB (73.96%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,11 +1,6 @@
 class Solution {
-static{
-    for(int i = 0 ; i<500; i++){
-        uniformArray(new int[]{1,2,3,4,5});
-    }
-}
-
     public static boolean uniformArray(int[] nums1) {
+        
         return true;
     }
 }
